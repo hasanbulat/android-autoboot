5dd069ce191ca08ca0a8b2b508f848c92bc4fa4c
Hasan Bulat <hasan@azenqos.com>
2021-02-15 13:35:55 +0700

    update script

--- autoboot-patcher.sh
+++ autoboot-patcher.sh
@@ -3,4 +3,19 @@
 MAGISK_BOOT=$1
 
 export PATH=bin:$PATH
+
+#clen
+echo "--- Cleaning..."
+magiskboot cleanup
+
+#unpack
+echo "--- Unpacking img..."
 magiskboot unpack $MAGISK_BOOT
+
+#ramdisk patch
+echo "--- Patching ramdisk..."
+magiskboot cpio ramdisk.cpio \
+"mkdir 0700 overlay.d" \
+"add 0700 overlay.d/init.custom.rc init.custom.rc" 
+echo "--- Packing img..."
+magiskboot repack $MAGISK_BOOT
