d66bb0246aaa5113c5f9912a2c04813e21c0c81b
Hasan Bulat <hasan@azenqos.com>
2021-02-15 14:45:15 +0700

    update script

--- autoboot-patcher.sh
+++ autoboot-patcher.sh
@@ -4,6 +4,15 @@ MAGISK_BOOT=$1
 
 export PATH=bin:$PATH
 
+if [ $1 ]; then
+    echo "--- use $1 as magisk patched boot image"
+else
+    echo "Failed!!! - Please choose magisk patched boot image"
+    echo "Usage:"
+    echo "       ./autoboot-patcher.sh <magisk_patched_boot.img>"
+    exit 1
+fi
+
 #clen
 echo "--- Cleaning..."
 magiskboot cleanup
@@ -19,3 +28,11 @@ magiskboot cpio ramdisk.cpio \
 "add 0700 overlay.d/init.custom.rc init.custom.rc" 
 echo "--- Packing img..."
 magiskboot repack $MAGISK_BOOT
+
+if [ -f new-boot.img ]; then
+    echo "### new-boot.img --- created"
+    echo "### use \"fastboot flash boot new-boot.img\" to flash boot image to devices"
+else
+    echo "### ERROR!!! --- patch failed"
+    exit 1
+fi
